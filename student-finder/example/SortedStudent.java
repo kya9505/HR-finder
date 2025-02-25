94f9f74c0a85a49ac88f58409baa5121979ab95d
KangChangSeon <rlckdtjs@naver.com>
2025-02-25 09:15:27 +0900

    commit

---
+++ student-finder/example/SortedStudent.java
@@ -0,0 +1,8 @@
+package example;
+
+import java.util.List;
+
+public interface SortedStudent {
+    void sortByTotal(List<Student> students);
+    void sortBySno(List<Student> students);
+}
---
+++ student-finder/example/StudentExitService.java
@@ -0,0 +1,9 @@
+package example;
+
+/**
+ * StudentExitService 인터페이스는 애플리케이션을 종료하는 기능을 정의합니다.
+ */
+@FunctionalInterface
+public interface StudentExitService {
+    void exitApp();
+}
