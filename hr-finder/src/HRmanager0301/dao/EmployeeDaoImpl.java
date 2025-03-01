4b84254527bea27b5423da9c2fabbba8ee0e4801
kya <dbsdk4211@naver.com>
2025-03-01 17:21:43 +0900

    feat: SearchDB 연결 작업 중 (미완성)

--- hr-finder/src/HRmanager0301/dao/EmployeeDaoImpl.java
+++ hr-finder/src/HRmanager0301/dao/EmployeeDaoImpl.java
@@ -1,4 +1,50 @@
 package HRmanager0301.dao;
 
+import HRmanager0301.dto.Employees;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.function.Function;
+
+import static java_advanced.src.Quest.dao.DBUtil.getConnection;
+
 public class EmployeeDaoImpl {
+
+    public <T> Employees findSEmployee(String subchoide , T serchvalue) {
+
+        Function<T, String> converter = v -> (v instanceof String) ? (String) v : String.valueOf(v);
+        String strValue = converter.apply(serchvalue); //String으로 변환
+
+        Employees employees = null;
+        try {
+            Connection conn = getConnection();
+            PreparedStatement pstmt = conn.prepareStatement("SELECT ? FROM STUDENT WHERE ? ");
+            pstmt.setString(1, subchoide);
+            pstmt.setString(2,serchvalue + " = " + serchvalue);
+
+            try (ResultSet rs = pstmt.executeQuery()) {
+                if (rs.next()) {
+                    Employees.EmployeesBuilder builder = new Employees.EmployeesBuilder()
+                            .employee_id()
+                            .first_name()
+                            .last_name()
+                            .
+
+                    Student.StudentBuilder builder = new Student.StudentBuilder()
+                            .sno(sno)
+                            .name(name);
+                    builder.addSubject("korean", korean);
+                    builder.addSubject("english", english);
+                    builder.addSubject("math", math);
+                    builder.addSubject("science", science);
+                    student = builder.build();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return student;
+    }
+
 }
--- hr-finder/src/HRmanager0301/service/EmployeeServiceImpl.java
+++ hr-finder/src/HRmanager0301/service/EmployeeServiceImpl.java
@@ -1,39 +1,73 @@
 package HRmanager0301.service;
 
+import HRmanager0301.dao.EmployeeDaoImpl;
 import HRmanager0301.dto.Employees;
 
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
+import java.util.Scanner;
 
 public class EmployeeServiceImpl {
-
-    ArrayList employeesList  = new ArrayList<Employees>();
+    private final EmployeeDaoImpl employeeDao = new EmployeeDaoImpl();
+    ArrayList employeesList = new ArrayList<Employees>();
+    Scanner sc = new Scanner(System.in);
 
     //사원 번호를 기준으로 검색
-    public void findByEmpId(int employee_id){
-
+    public void findByEmpId(int employee_id) {
+        int count = 0;
+        employee_id = sc.nextInt();
+        employeeDao.findSEmployee(serchSubMenu(),employee_id);
     }
 
     //이름으로 검색 - last name 검색
-    public void findByLastname(String last_name ){}
+    public void findByLastname(String last_name) {
+    }
+
     //이름 검색 - first name 검색
-    public void findByFirstname(String First_name){}
+    public void findByFirstname(String First_name) {
+    }
 
     //직업 검색
-    public void findByJobId(String job_id){}
+    public void findByJobId(String job_id) {
+    }
 
 
     //고용일/근속기간 검색 - 고용일 검색
-    public void findByHireDate(Date hire_date){}
+    public void findByHireDate(Date hire_date) {
+    }
+
     //고용일/근속기간 검색 - 근무기간 검색
-    public void findByEmploymentDuration(Date startDate, Date endDate){}
+    public void findByEmploymentDuration(Date startDate, Date endDate) {
+    }
+
     //고용일/근속기간 검색 - 연차 검색
-    public void getEmployeesByServiceYears(int minYears, int maxYears){}
+    public void getEmployeesByServiceYears(int minYears, int maxYears) {
+    }
+
     //근무지역으로 검색 - 지역으로 검색 department,location join
-    public void findByLocation(int location_id){}
+    public void findByLocation(int location_id) {
+    }
 
     //subMenu : 직원수만 확인 하거나 해당 직원의 정보를 조회할 수 있다.
-    public <T> void serchSubMenu(T serchReturn){}
+    public String serchSubMenu() {
+        while (true) {
+            System.out.println("1. Count  2. All Load");
+            String input = sc.nextLine();
 
+            try {
+                int subchoice = Integer.parseInt(input);
+                switch (subchoice) {
+                    case 1:
+                        return " count(employee_id) ";
+                    case 2:
+                        return " * ";
+                    default:
+                        System.out.println("re choice, 1 or 2");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("숫자를 입력하세요.");
+            }
+        }
+    }
 }
+
