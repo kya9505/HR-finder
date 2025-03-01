b26dbe42abc1bb8dc3f56d574b250fda80f6ac60
Eveieve <jhkdev1@gmail.com>
2025-03-01 16:16:35 +0900

    feat: add getIncreasedAnnualSalary()

--- hr-finder/src/HRmanager0228/service/SalaryService.java
+++ hr-finder/src/HRmanager0228/service/SalaryService.java
@@ -1,4 +1,6 @@
 package HRmanager0228.service;
 
 public interface SalaryService {
+    // Grade에 따라서
+    public double getIncreasedAnnualSalary(int employeeId, char performanceGrade);
 }
--- hr-finder/src/HRmanager0228/service/SalaryServiceImpl.java
+++ hr-finder/src/HRmanager0228/service/SalaryServiceImpl.java
@@ -1,4 +1,52 @@
 package HRmanager0228.service;
 
-public class SalaryServiceImpl {
+public class SalaryServiceImpl implements SalaryService {
+    @Override
+    public double getIncreasedAnnualSalary(int employeeId, char performanceGrade) {
+        char grade = getPerfGrade(employeeId); // 직원의 등급 가져오기
+        double annualSalary = employee.getSalary() * 12;
+
+        // 등급에 따른 연봉 인상률 매핑, 인상된 연봉을 반환
+        double increasedSalary = switch (grade) {
+            case 'S' -> annualSalary * 0.15; // 이 자체가 반환값
+            case 'A' -> annualSalary * 0.1;
+            case 'B' -> annualSalary * 0.55;
+            case 'C' -> annualSalary * 0.35;
+            case 'D' -> annualSalary * 0.01;
+            default -> {
+                System.out.println("Invalid grade. Please check the employee's performance grade again.");
+                yield 0.0;
+            }
+
+        };
+        return increasedSalary;
+    }
+
+    // 임직원 관리쪽의 직원 검색 메소드를 여기서 사용해도 되려나?
+    private Employee searchEmployee(int employeeId) {
+        for(Employee employee : employees) {
+            if(employee.getEmployeeId() == employeeId) {
+                return employee;
+            }
+        }
+        return null; // 못찾으면 null 반환.
+    }
+    // 특정 직원의 성과점수에 따라 등급을 매김. 프라이빗 메소드.
+    private char getPerfGrade(int employeeId) {
+        // id로 찾은 직원의 점수 조회.
+        int score = searchEmployee(employeeId).getPerfScore(); // 사원 객체의 평가 점수. // getPerfScore는 특정 직원의 점수 필드값.
+        int scoreOneDigit = score / 10; // 나머지 빼고 십의 자리만
+
+        // break 문이 필요 없는 enhanced switch
+        char grade = switch (scoreOneDigit) {
+            case 10 -> 'S';
+            case 9 -> (score >= 95) ? 'S' : 'A'; // 95 above is S
+            case 8 -> 'B';
+            case 7 -> (score >= 75) ? 'B' : 'C';
+            default -> 'D';
+        };
+
+        return grade;
+    }
+
 }
